123d33e91545962fa76ecc77fedeea1c3c4910dc
Arpit Malik <arpitmalik04@gmail.com>
2018-09-24 01:32:12 +0530

    Improved the file structure

--- server/index.js
+++ server/index.js
@@ -1,37 +1,11 @@
 const express = require("express");
-const passport = require("passport");
-const GoogleStrategy = require("passport-google-oauth20").Strategy;
-const keys = require("./config/keys");
+// To execute passport.js
+require('./services/passport');
 
 const app = express();
 
-passport.use(
-  new GoogleStrategy(
-    {
-      clientID: keys.googleClientID,
-      clientSecret: keys.googleClientSecret,
-      callbackURL: '/auth/google/callback'
-    },
-    (accessToken, refreshToken, profile, done) => {
-      console.log('accessToken', accessToken);
-      console.log('refreshToken', refreshToken);
-      console.log('profile', profile);
-    }
-  )
-);
-
-app.get('/', (req,res) => (
-  res.send({hey: 'there'})
-));
-
-app.get(
-  "/auth/google",
-  passport.authenticate('google', {
-    scope: ['profile', 'email']
-  })
-);
-
-app.get('/auth/google/callback', passport.authenticate('google'));
+// Since 'authRoutes' returns a function, with an immidiately invoked callback function 'app'.
+require('./routes/authRoutes')(app); 
 
 const PORT = process.env.PORT || 5000; //For heroku
 app.listen(PORT);
---
+++ server/routes/authRoutes.js
@@ -0,0 +1,18 @@
+const passport = require('passport');
+
+module.exports = (app)=> {
+
+    app.get('/', (req,res) => (
+        res.send({hey: 'there'})
+    ));
+    
+    // Testing google OAuth
+    app.get(
+        "/auth/google",
+        passport.authenticate('google', {
+        scope: ['profile', 'email']
+        })
+    );
+    
+    app.get('/auth/google/callback', passport.authenticate('google'));
+}
